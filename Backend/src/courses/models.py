0a5445787535faa4bf09300a8cdd94a5fec7f585
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-02-09 05:26:10 +0200

    final design for the courses model

--- Backend/src/courses/models.py
+++ Backend/src/courses/models.py
@@ -8,10 +8,10 @@ class Courses(models.Model):
     subtitle = models.CharField(max_length=100)
     description = models.TextField(max_length=400)
     price = models.DecimalField(max_digits=5,decimal_places=2)
-    categories= models.OneToOneField('Categories ', models.SET_NULL,blank=True, null=True)
+    categories= models.ForeignKey('Categories', models.SET_NULL,blank=True, null=True)
     
 class Reviews(models.Model):
-    user = models.ForeignKey(User,related_name='reviews_course',on_delete=models.SET_NULL)
+    user = models.ForeignKey(User,related_name='reviews_course',on_delete=models.SET_NULL,null=True)
     course = models.ForeignKey(Courses,related_name='reviews_course',on_delete=models.CASCADE)
     review = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
