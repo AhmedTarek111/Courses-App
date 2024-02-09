a0c64d9a501c05b45bbcce42a2d06ec4dd75ebb1
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-02-09 21:00:32 +0200

    add course , review categories serializers

--- Backend/src/courses/serializers.py
+++ Backend/src/courses/serializers.py
@@ -1,1 +1,17 @@
-import serializers
+from rest_framework import serializers
+from .models import Categories,Courses,Review
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta():
+        model =Courses
+        fields = ['name','subtitle','description','price','category',]
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta():
+        model =Review
+        fields =['course','review']
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta():
+        model =Categories
+        fields =['name']
